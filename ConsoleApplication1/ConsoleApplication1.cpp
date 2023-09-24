42368cbd3dfffae629a4aab52cfe7df01db12733
я <@DESKTOP-SK9PR6E>
2023-09-24 18:08:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Some code will be here soon)
+}
+
+struct Pipe
+{
+	string name;
+	int length;
+	int diametr;
+	bool is_repairing = false;
+};
+
+struct Compress_station
+{
+	string name;
+	int shops_num;
+	int busy_shops_num;
+	int efficiency;
+};
